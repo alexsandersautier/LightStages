65cfc15dfd0f0e46dfa16029c31ffcdaa22242e5
Alexsander Sautier <alexsanderesramos@gmail.com>
2024-04-29 23:20:18 -0300

    add logical broken

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,12 +2,20 @@ const on = document.getElementById('on')
 const off = document.getElementById('off')
 const lamp = document.getElementById('lamp')
 
+
+let broken = false
 on.addEventListener('click', () => {
-    lamp.setAttribute('src', 'assets/imgs/on.svg') 
+    if(!broken){
+        lamp.setAttribute('src', 'assets/imgs/on.svg') 
+    }
 })
 off.addEventListener('click', () => {
-    lamp.setAttribute('src', 'assets/imgs/off.svg') 
+    if(!broken){
+        lamp.setAttribute('src', 'assets/imgs/off.svg') 
+    }
 })
+console.log(broken)
 lamp.addEventListener('click', () => {
     lamp.setAttribute('src', 'assets/imgs/broken.svg') 
+    broken = true
 })
